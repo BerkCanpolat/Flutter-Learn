b44fd96f9b49d319bfc3d2d3e2f917b5024659da
Berk Canpolat <canpolatberk19@gmail.com>
2024-06-29 22:14:36 +0300

    Eleventh day added

---
+++ Bootcamp/onbirinci_gun/kisiler_uygulamasi-dio/lib/ui/views/kayit_sayfa.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:kisiler_uygulamasi/ui/cubit/kayit_cubit.dart';
+
+class KayitSayfa extends StatefulWidget {
+  const KayitSayfa({super.key});
+
+  @override
+  State<KayitSayfa> createState() => _KayitSayfaState();
+}
+
+class _KayitSayfaState extends State<KayitSayfa> {
+  var tfKisiAdi = TextEditingController();
+  var tfKisiTel = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Kayıt Sayfası"),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 50, right: 50),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              TextField(
+                controller: tfKisiAdi,
+                decoration: InputDecoration(
+                  hintText: "Kişi Ad",
+                ),
+              ),
+              TextField(
+                controller: tfKisiTel,
+                decoration: InputDecoration(
+                  hintText: "Kişi Tel",
+                ),
+              ),
+              ElevatedButton(onPressed: (){
+                context.read<KayitSayfaCubit>().kaydet(tfKisiAdi.text, tfKisiTel.text);
+              }, child: const Text("Kaydet"))
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
