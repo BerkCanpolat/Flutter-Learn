c833d85fda7e24a1af0aa1ef3c9f762047b0b817
Berk Canpolat <canpolatberk19@gmail.com>
2024-07-06 15:25:30 +0300

    week three and list added

---
+++ Veli_Bacik/dart_lang_full_course/week3/list.dart
@@ -0,0 +1,64 @@
+void main() {
+  //LİSTELER BİR REFERANS TİPİDİR.
+  // Bir bankaya on müşteri gelir bunların 100 tl'si 110tl 50tl'si vardır
+  final int money1 = 100;
+  final int money2 = 110;
+
+  // 100'den büyük olanlara hoşgeldiniz beyfendi
+  if(money1 > 100) {
+    print("Beyefendi");
+  }
+
+  if(money2 > 100) {
+    print("aa");
+  }
+
+
+  String name = '';
+
+  List<int> moneys = [100,110,500, 200];
+  final List<int> newMoneys = [100,110,500, 200];
+
+  print("Müştei 1 parası: ${moneys[0]}");
+
+  // müşterileri parası büyük olana göre almak istiyorum
+
+  //Sort elemanları sıralar.
+  moneys.sort();
+  //Add methodu sıralamanın en sonuna ekleme yapar.
+  moneys.add(5);
+  //insert edersek eğer 2.Sıraya 300 yaz demiş oluruz.
+  moneys.insert(2, 300);
+  print(moneys);
+
+  newMoneys.add(4);
+  //Ana listeyi etkilemeden elimizde yeni bir liste oluşmuş oluyor.
+  newMoneys.reversed.toList();
+  print(newMoneys);
+
+  // 100 tane müşteri yap bunların hepsine sıra ile numarasına göre 5 tl ekle
+
+  List<double> customerMoney = List.generate(100, (index) {
+    return index + 5;
+  });
+  print(customerMoney);
+
+  // Customers 100 30 40 60
+  // 35 tl den büyük olanlara burada kredi verebiliriz
+  // Küçük olanlara bye yazdır
+
+  List<int> moneyCustomerNews = [100,30,40,60];
+
+  moneyCustomerNews.sort();
+
+  for (int i = 0; i < moneyCustomerNews.length; i += 1) {
+    print('müşteri parası ${moneyCustomerNews[i]}');
+    if (moneyCustomerNews[i] > 35) {
+      print("Kredi hazır");
+    } else if (moneyCustomerNews[i] > 0) {
+      print("Kredi veremeyiz ama bi bakalım");
+    } else {
+      print("Bye");
+    }
+  }
+}
