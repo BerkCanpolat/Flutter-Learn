2f66f74a6e4b0853d94decc548e0ab5ebc1c6e7f
Berk Canpolat <canpolatberk19@gmail.com>
2024-11-03 13:24:22 +0300

    Flutter architechture mobx added

--- Veli_Bacik/flutter_architecture/lib/view/authenticate/test/view/test_view.dart
+++ Veli_Bacik/flutter_architecture/lib/view/authenticate/test/view/test_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_architecture/core/base/state/base_state.dart';
 import 'package:flutter_architecture/core/base/view/base_widget.dart';
+import 'package:flutter_architecture/view/authenticate/test/viewmodel/test_view_model.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 
 class TestView extends StatefulWidget {
   const TestView({super.key});
@@ -10,11 +12,24 @@ class TestView extends StatefulWidget {
 }
 
 class _TestViewState extends BaseState<TestView> {
+  late TestViewModel viewModel;
   @override
   Widget build(BuildContext context) {
-    return BaseView(
-      viewModel: "viewModel", 
+    return BaseView<TestViewModel>(
+      viewModel: TestViewModel(), 
+      onModelReady: (model) {
+        viewModel = model;
+      },
       onPageBuilder: (context, value) => Text('data'),
     );
   }
+
+  Widget get scaffoldBody => Scaffold(
+    floatingActionButton: floatingActionButtonNumberIncrement,
+    body: textNumber,
+  );
+
+  FloatingActionButton get floatingActionButtonNumberIncrement => FloatingActionButton(onPressed: () => viewModel.incrementNumber);
+
+  Widget get textNumber => Observer(builder: (context) => Text(viewModel.number.toString()));
 }
