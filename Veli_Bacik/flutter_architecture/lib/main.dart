54bcfbb8f616ced30888485130a5b8d93257a66a
Berk Canpolat <canpolatberk19@gmail.com>
2024-11-03 22:55:37 +0300

    App theme and providers added

--- Veli_Bacik/flutter_architecture/lib/main.dart
+++ Veli_Bacik/flutter_architecture/lib/main.dart
@@ -2,16 +2,24 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_architecture/core/constants/app/app_constants.dart';
 import 'package:flutter_architecture/core/init/lang/lang_manager.dart';
+import 'package:flutter_architecture/core/init/notifier/provider_list.dart';
+import 'package:flutter_architecture/core/init/notifier/theme_notifier.dart';
 import 'package:flutter_architecture/view/authenticate/test/view/test_view.dart';
+import 'package:provider/provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
-  runApp(EasyLocalization(child: MyApp(),
-  supportedLocales: LanguageManager.instance.supportedLocales, 
-  path: ApplicationConstants.LANG_ASSET_PATH,
-  //fallbackLocale: LanguageManager.instance.enLocale,
-  startLocale: LanguageManager.instance.enLocale,
+  runApp(MultiProvider(
+    providers: [
+      ...ApplicationProvider.instance.dependItems
+    ],
+    child: EasyLocalization(child: MyApp(),
+    supportedLocales: LanguageManager.instance.supportedLocales, 
+    path: ApplicationConstants.LANG_ASSET_PATH,
+    //fallbackLocale: LanguageManager.instance.enLocale,
+    startLocale: LanguageManager.instance.trLocale,
+    ),
   ));
 }
 
@@ -22,10 +30,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      theme: context.watch<ThemeNotifier>().currentTheme,
       localizationsDelegates: context.localizationDelegates,
       supportedLocales: context.supportedLocales,
       locale: context.locale,
