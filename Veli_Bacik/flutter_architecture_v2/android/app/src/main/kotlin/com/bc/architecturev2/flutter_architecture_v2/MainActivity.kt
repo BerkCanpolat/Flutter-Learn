e9a59a77aef2b1d4b1aa1749493d432cd52f3850
Berk Canpolat <canpolatberk19@gmail.com>
2024-11-08 12:46:08 +0300

    Flutter Architecture V2 Started

---
+++ Veli_Bacik/flutter_architecture_v2/android/app/src/main/kotlin/com/bc/architecturev2/flutter_architecture_v2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.bc.architecturev2.flutter_architecture_v2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ Veli_Bacik/flutter_architecture_v2/lib/feature/home/view/mixin/home_view_mixin.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_architecture_v2/feature/home/view/home_view.dart';
+
+mixin HomeViewMixin on State<HomeView> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+}
