7cef3d7e28ca75882d734b4483a3032cf5d1c25b
Berk Canpolat <canpolatberk19@gmail.com>
2024-11-18 23:09:03 +0300

    minor change

--- Veli_Bacik/flutter_architecture_v2/lib/product/service/manager/product_network_error_manager.dart
+++ Veli_Bacik/flutter_architecture_v2/lib/product/service/manager/product_network_error_manager.dart
@@ -9,7 +9,29 @@ final class ProductNetworkErrorManager {
 
   void handleError(int value) {
     if(value == HttpStatus.unauthorized) {
-      
+
     }
   }
 }
+
+
+
+// final class ProductNetworkErrorManager {
+//   final BuildContext context;
+//   ProductNetworkErrorManager(this.context);
+
+//   void handleError(int value) {
+//     if (value == HttpStatus.unauthorized) {
+//       // Kullanıcı oturumu geçersizse (401)
+//       print('Unauthorized error');
+//       // Burada oturum açma ekranına yönlendirme yapılabilir
+//     } else if (value == HttpStatus.notFound) {
+//       // İstek için doğru uç nokta bulunamadıysa (404)
+//       print('Not Found error');
+//       // Burada bir hata mesajı gösterebilirsin
+//     } else {
+//       // Diğer durum kodları için
+//       print('Error: $value');
+//     }
+//   }
+// }
