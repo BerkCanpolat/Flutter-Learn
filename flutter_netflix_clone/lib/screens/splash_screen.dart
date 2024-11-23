65ca24b684d9c0a02c7ccca9feaf7d6020ed8d28
Berk Canpolat <canpolatberk19@gmail.com>
2024-11-23 23:48:52 +0300

    Splash Screen Add

---
+++ flutter_netflix_clone/lib/screens/splash_screen.dart
@@ -0,0 +1,32 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      //Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => BottomNavBar()));
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Lottie.asset('assets/netflix.json'),
+    );
+  }
+}
