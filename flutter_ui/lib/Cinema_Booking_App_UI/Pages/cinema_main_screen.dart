5eb5bc042d62ba8b0b9c348968ec596ac1b7b1fd
Berk Canpolat <canpolatberk19@gmail.com>
2025-03-05 15:27:31 +0300

    Flutter cinema booking app ui start

---
+++ flutter_ui/lib/Cinema_Booking_App_UI/Pages/cinema_main_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CinemaMainScreen extends StatefulWidget {
+  const CinemaMainScreen({super.key});
+
+  @override
+  State<CinemaMainScreen> createState() => _CinemaMainScreenState();
+}
+
+class _CinemaMainScreenState extends State<CinemaMainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
